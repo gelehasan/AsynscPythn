26559c899d24a5dae35114034d9e98aa03ae5cbd
Gele hasan <gellehussen@gmail.com>
2024-07-29 14:12:36 +0200

    made the chat server respond to requests

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'chat/index.html')
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {'room_name': room_name})
+
+# Create your views here
